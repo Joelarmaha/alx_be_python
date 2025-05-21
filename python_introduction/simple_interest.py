41703215928a930c8b0a29ad06a4005b9f703c9d
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-05-21 10:28:27 +0000

    simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,10 @@
-principal = 1000
-rate = 0.05
-time = 3
+# I = Interest
+# P = principal
+# R = rate
+# T = time
 # I = P * R * T
-P = principal
-R= rate
-T= time
+P = 1000
+R = 0.05
+T = 3
 interest =int(P) * float(R) * int(T)
 print(f" the simple interest is : {interest}")
