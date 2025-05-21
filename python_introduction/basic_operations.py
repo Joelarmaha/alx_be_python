e80c5ea821af335f823ec1b5179b17b47cac8f60
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-05-21 09:26:46 +0000

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = int(10)
+number2 = int(5)
+result = number1 + number2
+result2 = number1 - number2
+result3 = number1 * number2
+print(f"addition of {number1} and {number2} is {result}")
+print(f"subtraction of {number1} and {number2} is {result2}")
+print(f"multiplication of {number1} and {number2} is {result3}")
