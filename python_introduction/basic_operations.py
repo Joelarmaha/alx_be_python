268b576f6c6c7c5edf1dd36b4a5806d6caedd417
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-05-21 09:37:38 +0000

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-number1 = int(10)
-number2 = int(5)
-result = number1 + number2
-result2 = number1 - number2
-result3 = number1 * number2
+number1 = 10
+number2 = 5
+result = int(number1) + int(number2)
+result2 = int(number1) - int(number2)
+result3 = Int(number1) * int(number2)
 print(f"addition of {number1} and {number2} is {result}")
 print(f"subtraction of {number1} and {number2} is {result2}")
 print(f"multiplication of {number1} and {number2} is {result3}")
