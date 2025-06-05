dfaf613195c1144d931f6a8757c9f9e784990a1e
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-06-05 22:51:58 +0000

    shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,44 @@
+def shopping_list():
+    shopping_list = []
+
+    if shopping_list == '1':
+        return shopping_list.append['']
+    elif shopping_list == '2':
+        return shopping_list.remove['']
+    elif operation == '3':
+        return shopping_list("")
+    else:
+        return "this is not valid"
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
