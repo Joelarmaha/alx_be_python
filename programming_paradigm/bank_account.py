edccd6f9fa48cde1c56fc7565ef6eb42feaeb2df
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-06-11 15:45:53 +0000

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,10 +18,10 @@ class BankAccount:
             return False
         if amount <= self.__account_balance:
             self.__account_balance -= amount
-            print(f"Withdrawn: ${amount:.2f}")
+            print(f"Withdrew: ${amount:.2f}")
             return True
         else:
-            print("Insufficient funds for this withdrawal.")
+            print("Insufficient funds.")
             return False
 
     def display_balance(self):
