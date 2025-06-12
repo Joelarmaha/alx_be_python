ee659ddf701c1a3024e00282c0aeb22456543e13
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-06-12 09:03:37 +0000

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,33 +1,32 @@
 class BankAccount:
-    def __init__(self, account_balance, withdraw, deposit, current_balance):
-        self.display_balance = None
-        self.account_balance = account_balance
-        self.deposit = deposit
-        self.withdraw = withdraw
-        self.current_balance = current_balance
+    def __init__(account, account_balance, withdraw, deposit, current_balance):
+        account.display_balance = None
+        account.account_balance = account_balance
+        account.deposit = deposit
+        account.withdraw = withdraw
+        account.current_balance = current_balance
 
-    def deposit(self, amount):
+    def deposit(account, amount):
         if amount > 0:
-            self.account_balance = self.account_balance + amount
+            account.account_balance = account.account_balance + amount
             return f' Deposited: {amount}'
         else:
             return "not valid"
 
-    def withdraw(self, amount):
+    def withdraw(account, amount):
         if amount != 0:
-            self.account_balance = self.account_balance - amount
+            account.account_balance = account.account_balance - amount
             print(f' Deposited: {amount}')
         else:
             return "not valid"
 
-    def display_balance(self, current_balance):
-        self.display_balance = current_balance
-        return f' Current Balance: {self.display_balance()}'
+    def display_balance(account, current_balance):
+        account.display_balance = current_balance
+        return f' Current Balance: {account.display_balance()}'
 
 
 import sys
 from bank_account import BankAccount
-
 
 def main():
     account = BankAccount(100)  # Example starting balance
@@ -51,7 +50,6 @@ def main():
         account.display_balance()
     else:
         print("Invalid command.")
-
 
 if __name__ == "__main__":
     main()
