6e23dacea5ac34422ca3d4f0a2e7e704bcc80fcd
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-06-13 11:28:03 +0000

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -34,7 +34,7 @@ def main():
     elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
             print(f"Withdrew: ${amount}")
-        elif amount > float(BankAccount):
+        elif amount > float(account):
             print("Insufficient funds.")
         else:
             print("not valid.")
