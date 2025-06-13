46f09ad8f120fbefb2f67774862bc9350b480849
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-06-13 12:09:06 +0000

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,7 +14,7 @@ class BankAccount:
             self.account_balance -= amount
             return f"Withdrew:${amount}"
         else:
-            return "Insufficient funds."
+            print("Insufficient funds.")
 
     def display_balance(self):
         return f"Current Balance:${self.account_balance}"
